c394f00b7b446ed470e20c67354901974a0d3c7f
1vsALL <alikkv@mail.ru>
2024-05-22 01:56:53 +0300

    ненавижу!!!!!111

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @GetMapping()
+    public String showSimpleUser(Principal principal, ModelMap modelMap) {
+        User user = userRepository.findByName(principal.getName()).get();
+        modelMap.addAttribute("userID", user);
+        return "userShow";
+    }
+}
